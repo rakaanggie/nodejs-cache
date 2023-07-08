e4d5ff580401b98a62b82ff283995601f4f83bfe
rakaanggie <114036721+rakaanggie@users.noreply.github.com>
2023-07-08 08:39:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const fetch = require('node-fetch');
 const redis = require('redis');
 
 const PORT = process.env.PORT || 5000;
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_HOST = process.env.REDIS_HOST || 6379;
 
-const client = redis.createClient(REDIS_PORT);
+const client = redis.createClient(REDIS_HOST);
 const app = express();
 
 //set setRseponse
